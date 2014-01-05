a642d109c866fa5755e4c60b7a6eb7a604f8a771
Masaki Hirokawa <info@dolice.net>
2014-01-05 12:04:46 +0900

    Add all files

---
+++ DCWebViewSample/DCWebViewSample/Classes/Screen/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DCWebViewSample
+//
+//  Created by Dolice on 2014/01/05.
+//  Copyright (c) 2014年 Dolice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DCWebView.h"
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DCWebViewSample/DCWebViewSample/Classes/Screen/ViewController.m
@@ -0,0 +1,52 @@
+//
+//  ViewController.m
+//  DCWebViewSample
+//
+//  Created by Dolice on 2014/01/05.
+//  Copyright (c) 2014年 Dolice. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    // ウェブビュー配置
+    [self setWebView];
+}
+
+#pragma mark - Web View
+
+// ウェブビュー配置
+- (void)setWebView
+{
+    // ウェブビュー生成
+    DCWebView *webView = [[DCWebView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
+    
+    // 背景色指定 (初期値: [UIColor whiteColor])
+    webView.backgroundColor = [UIColor whiteColor];
+    
+    // ピンチイン・ピンチアウトの可否を指定 (初期値: NO)
+    webView.scalesPageToFit = NO;
+    
+    // 背景を透過するか (初期値: NO)
+    webView.opaque = NO;
+    
+    // 画面上下の端でバウンスしないよう指定
+    [webView bounces:NO];
+    
+    // URL読み込み
+    [webView loadUrl:@"http://dolice.net/mb/" view:self.view];
+    
+    // ビューに追加
+    [self.view addSubview:webView];
+}
+
+@end
